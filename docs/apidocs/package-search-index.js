d425ef28cdabe442239e3771f718ad9ecad9b3b3
narenberg <nick.arenberg22@gmail.com>
2019-12-02 16:56:03 -0800

    na - updated jacoco and javadocs

--- docs/apidocs/package-search-index.js
+++ docs/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hello"},{"l":"hello.geojson"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"earthquakes"},{"l":"earthquakes.controllers"},{"l":"earthquakes.entities"},{"l":"earthquakes.geojson"},{"l":"earthquakes.repositories"},{"l":"earthquakes.searches"},{"l":"earthquakes.services"}]

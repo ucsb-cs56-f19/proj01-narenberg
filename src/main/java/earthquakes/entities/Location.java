7ac637abb1fa4bbfa1576a460f30938a553bddee
Nick Arenberg <narenberg@ucsb.edu>
2019-12-04 18:37:01 -0800

    fixed longitude showing as latitude

--- src/main/java/earthquakes/entities/Location.java
+++ src/main/java/earthquakes/entities/Location.java
@@ -30,6 +30,6 @@ public class Location {
     public double getLatitude() { return latitude; }
     public void setLatitude (double latitude) { this.latitude = latitude; }
 
-    public double getLongitude() { return latitude; }
-    public void setLongitude (double longitude) { this.latitude = latitude; }
+    public double getLongitude() { return longitude; }
+    public void setLongitude (double longitude) { this.longitude = longitude; }
 }
